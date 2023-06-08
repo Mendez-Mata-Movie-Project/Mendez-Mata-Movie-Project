097d57bf60ebfde5b3d981f27eaec74f439d8180
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-06-08 16:58:33 -0500

    Working on the movie card layout

--- js/script.js
+++ js/script.js
@@ -15,15 +15,19 @@ $.ajax({
             .then(omdbData => {
                 let poster = omdbData.Poster;
                 movieListHtml += `
-                  <div class="card ">
+                  <div class="card">
+                    <div class="rating-overlay">
+                      <p class="card-text card-rating p-0">${movie.rating}</p>
+                    </div>
                     <img src="${poster}" class="card-img-top" alt="${movie.title}" width="200" height="275">
                     <div class="card-body">
-                      <p class="card-title wrap-text">${movie.title}</p>
-                      <p class="card-text">Rating: ${movie.rating}</p>
+                    <p class="card-title wrap-text d-inline text-center">${movie.title}</p>
+                      <div class="d-flex justify-content-end align-self-end">
+                          <i class="fa-regular fa-pen-to-square edit-icon" data-movie-id="${movie.id}"></i>
+                          <i class="fa-solid fa-trash" data-movie-id="${movie.id}"></i>
+                      </div>
                     </div>
                   </div>`;
-
-
                 $('#movies-list').html(movieListHtml);
             })
             .catch(error => {
